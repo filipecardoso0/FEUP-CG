1a41f406a9066e4228968304f664ba91d828dfaa
Joao Araujo <up202004293@edu.fe.up.pt>
2023-04-17 23:40:28 +0100

    proj-2

--- project/MyPanoram.js
+++ project/MyPanoram.js
@@ -5,15 +5,19 @@ export class MyPanoram extends CGFobject {
     constructor(scene, CGFtexture) {
         super(scene);
         this.texture = CGFtexture;
-        this.appearancePan = new CGFappearance(this.scene);
-        this.appearancePan.setTexture(this.texture);
-        this.appearancePan.setTextureWrap('REPEAT', 'REPEAT');
+        this.appearance = new CGFappearance(this.scene);
+        this.appearance.setAmbient(1.0, 1.0, 1.0, 1.0);
+		this.appearance.setDiffuse(1.0, 1.0, 1.0, 1.0);
+		this.appearance.setSpecular(0.0, 0.0, 0.0, 1.0);
+        
+        this.appearance.setTexture(this.texture);
+        this.appearance.setTextureWrap('REPEAT', 'REPEAT');
 
         this.sphere = new MySphere(this.scene, 24 , 24, 200);
     }
     display() {
         this.scene.pushMatrix();
-        this.appearancePan.apply();
+        this.appearance.apply();
         this.scene.scale(this.scene.scaleFactor,this.scene.scaleFactor,this.scene.scaleFactor);
         this.sphere.display();
         this.scene.popMatrix();
