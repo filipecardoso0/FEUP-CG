07039b94b9e0aed6871b681e7a27fce3f4940f61
“Joao <up202004293@edu.fe.up.pt>
2023-05-15 11:11:35 +0100

    updated bird egg

--- project/objects/MyBirdEgg.js
+++ project/objects/MyBirdEgg.js
@@ -15,14 +15,15 @@ export class MyBirdEgg extends CGFobject {
         this.appearance.setTextureWrap('REPEAT', 'REPEAT');
 
         this.topsphere = new MyHollowOutsideHalfSphereTop(this.scene, 10, 5, 5); 
-        this.bottomsphere = new MyHollowOutsideHalfSphere(this.scene, 10, 5, 5);
+        this.bottomsphere = new MyHollowOutsideHalfSphereTop(this.scene, 10, 5, 5);
     }
 
     display(x, y, z) {
         //Top Part of the Egg
         this.scene.pushMatrix();
         this.appearance.apply();
-        this.scene.translate(x, y+(this.topsphere.radius)-1.5, z); 
+        this.scene.translate(x, y+this.topsphere.radius, z); 
+        this.scene.rotate(Math.PI, 1, 0, 0);
         this.scene.scale(1, 1.5, 1); 
         this.topsphere.display(); 
         this.scene.popMatrix();
